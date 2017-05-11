995d34359c99fa9770d191f8a7c5c9625debb7e1
Tomás Andrés Flores Alvear <tomfloresa@Tomass-MBP.home>
2017-05-11 20:59:13 -0300

    first commit

---
+++ src/components/posts_index.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions';
+
+class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    console.log(this.props.posts);
+    return(
+      <div>
+        Post Index
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    posts: state.posts
+  };
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
